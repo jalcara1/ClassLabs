1a7fa040378b888880afecb9f98084eec8b19331
Juan Pablo Alcaraz Florez <jalcara1@eafit.edu.co>
2018-06-03 21:05:06 -0500

    Multi Threading For Each Image

--- Lab3/LicensePlateRecognition.py
+++ Lab3/LicensePlateRecognition.py
@@ -1,46 +1,70 @@
 #!/usr/bin/python3
 
-import multiprocessing
-import subprocess
+import multiprocessing 
 import boto3
-import time
 import json
 import cv2
-import sys
+import re
 
-from PIL import Image
-   
+def amazon_aws(image):
+    s3=boto3.resource('s3')
+    bucket='telematicaequipo3'
+    s3=boto3.resource('s3')
+    photo=image
+    client=boto3.client('rekognition')
+    s3.meta.client.upload_file(photo, bucket, photo)
+    response=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
+    textDetections=response['TextDetections']
+    print("Image Processed")
+    for text in textDetections:
+        print ('Detected text:' + text['DetectedText'])
+        print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
+
 def license_plate_recognition():
-    #s3 = boto3.resource('s3')
-    bucket=''
-    fileName='Nothing'
+    faceCascade = cv2.CascadeClassifier('haarcascade_cars.xml')
+    video_capture = cv2.VideoCapture(0)
     cpt = 0
-    #client=boto3.client('','')
-    
-    #s3.meta.client.upload_file(fileName, bucket, fileName)
-    #response = client.detect_faces(Image={'S3Object':{'Bucket':bucket,'Name':fileName}},Attributes=['ALL'])
-    #source_img = s3.Object(bucket, fileName).get()
-    cap = cv2.VideoCapture(0)
-    while(True):
+
+    while True:
         # Capture frame-by-frame
-        ret, frame = cap.read()
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-        cv2.imshow('Recording Video', rgb)
-        #Taking Photo
-        #time.sleep(1)
-        cv2.imwrite("image%04i.jpg" %cpt, frame)
-        cpt += 1
-        print("Image Written!")
+        ret, frame = video_capture.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+        photo = str(faces)
+        print("---->> " + photo + " ------ " + str(len(photo)))
+        
+        if len(photo) > 4:
+            picture = "image%04i.jpg" %cpt
+            cv2.imwrite(picture, frame)
+            
+            thread = multiprocessing.Process(target=amazon_aws, args=(picture,))
+            thread.start()
+            
+            cpt += 1
+            print("Image Written!")
+        
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # Display the resulting frame
+        frame_copy = cv2.flip(frame, 1)
+        cv2.imshow('Video', frame_copy)
+        fps = video_capture.get(cv2.CAP_PROP_FPS)
+        print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    # When everything done, release the capture
-    cap.release()
+    # When everything is done, release the capture
+    video_capture.release()
     cv2.destroyAllWindows()
-    
+
 def main():
     license_plate_recognition()
-    
+
 if __name__ == '__main__':
     main()
-
-
